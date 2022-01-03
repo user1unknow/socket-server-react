1e950bfcbf12d2fa626371de281d2af89dfee953
user1unknow <userunk546@gmail.com>
2022-01-03 18:39:21 -0500

    Add Cors

--- models/server.js
+++ models/server.js
@@ -4,6 +4,7 @@ const http = require("http");
 const socketio = require("socket.io");
 const path = require("path");
 const Sockets = require("./sockets");
+const cors = require("cors")
 
 class Server {
 	constructor() {
@@ -30,6 +31,9 @@ class Server {
 	middlewares() {
 		//Desplegar el directorio publico
 		this.app.use(express.static(path.resolve(__dirname, "../public")));
+		
+		//CORS
+		this.app.use(cors())
 	}
 
 	execute() {
